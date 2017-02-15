709509ece80426112787e0b4971f4d31014ba820
Caleb Paul <calebpaul21@gmail.com>
2017-02-15 08:44:46 -0800

    add butterknife to app dependencies and add empty query activity

---
+++ app/src/main/java/calebpaul/quietpocket/ui/QueryActivity.java
@@ -0,0 +1,15 @@
+package calebpaul.quietpocket.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import calebpaul.quietpocket.R;
+
+public class QueryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query);
+    }
+}
